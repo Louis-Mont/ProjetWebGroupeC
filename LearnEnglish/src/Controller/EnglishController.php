62157ade01455c1265457f4f3f9b2b9277f8a10e
Simon <sim_thauvin@hotmail.fr>
2021-04-04 17:17:45 +0200

    create

--- LearnEnglish/src/Controller/EnglishController.php
+++ LearnEnglish/src/Controller/EnglishController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Lecon;
+use App\Repository\LeconRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,12 +11,16 @@ use Symfony\Component\Routing\Annotation\Route;
 class EnglishController extends AbstractController
 {
     /**
-     * @Route("/lecon", name="lecon")
+     * @Route("/english", name="english")
      */
-    public function index(): Response
+    public function index(LeconRepository $repo): Response
     {
+
+
+        $lecons = $repo->findAll();
         return $this->render('english/index.html.twig', [
             'controller_name' => 'EnglishController',
+            'lecons' => $lecons
         ]);
     }
 
@@ -26,12 +32,27 @@ class EnglishController extends AbstractController
         return $this->render('english/home.html.twig',['title'=>"Welcome"]);
     }
 
+
     /**
-     * @Route ("/lecon/12", name="lecon_show")
-     */
-    public function show (){
-        return $this->render('english/show.html.twig');
+    * @Route ("/english/new)", name="english_create")
+     **/
+    public function create(){
+        return $this->render('english/create.html.twig');
     }
+
+    /**
+     * @Route ("/english/{id}", name="english_show")
+     */
+    public function show (Lecon $lecon){
+
+
+        return $this->render('english/show.html.twig',[
+            'lecon' => $lecon
+        ]);
+
+    }
+
+
 
 
 }
